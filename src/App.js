fa80769d00c1ce49c0ded491763f8b1706e85204
Clebert Civil <clebertcivil01@gmail.com>
2025-05-25 01:00:07 -0400

    create my nav menu and link to their pages(components)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
- 
-import './App.css';
+import { BrowserRouter, Link, Routes, Route} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import todo from "./components/todo/todo";
+import contact from "./components/contact/contact";
+import about from "./components/about/about";
 
 function App() {
   return (
     <div className="App">
+      <h2>hello react</h2>
 
+      <BrowserRouter>
+        <ul>
+          <li><Link to="/"> homme</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+        <Routes>
+          <Route path="/" Component={todo}></Route>
+          <Route path="/contact" Component={contact}></Route>
+          <Route path="/about" Component={about}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
