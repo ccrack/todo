6c4ca18dfc836eb1b259aad7e40a89afc3141498
Clebert Civil <clebertcivil01@gmail.com>
2025-06-10 04:21:38 -0400

    create Footer component separete  the navbar to my app by create a Navbar component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,11 @@
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import React from "react";
 import "./App.css";
-import todo from "./Pages/todo/Todo";
-import contact from "./Pages/contact/Contact";
-import about from "./Pages/about/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <nav>
-          <div className="hamburger">
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-          </div>
-          <ul className="menu">
-            <li className="menu-item">
-              <Link className="link" to="/">
-                TODO
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link className="link" to="/contact">
-                CONTACT
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link className="link" to="/about">
-                ABOUT
-              </Link>
-            </li>
-          </ul>
-          <h2 className="logo-name">TODO LIST APP</h2>
-        </nav>
-
-        <Routes>
-          <Route path="/" Component={todo}></Route>
-          <Route path="/contact" Component={contact}></Route>
-          <Route path="/about" Component={about}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import todo from "../Pages/todo/Todo";
+import contact from "../Pages/contact/Contact";
+import about from "../Pages/about/About"
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <BrowserRouter>
+        <nav>
+          <div className="hamburger">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+          <ul className="menu">
+            <li className="menu-item">
+              <Link className="link" to="/">
+                TODO
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link className="link" to="/contact">
+                CONTACT
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link className="link" to="/about">
+                ABOUT
+              </Link>
+            </li>
+          </ul>
+          <h2 className="logo-name">TODO LIST APP</h2>
+        </nav>
+
+        <Routes>
+          <Route path="/" Component={todo}></Route>
+          <Route path="/contact" Component={contact}></Route>
+          <Route path="/about" Component={about}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
