3005bad6c4db4814f41e98890ea892aa59f2641c
Clebert Civil <clebertcivil01@gmail.com>
2025-06-10 03:31:57 -0400

    create handleChange and handleSubmit fuction to my form contact

--- src/Pages/contact/contact.jsx
+++ src/Pages/contact/contact.jsx
@@ -1,32 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.css";
 
-const handleChange = (e) => {};
-const handleSubmit = () => {};
+const Contact = () => {
+  //create state for each form field
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comment: "",
+  });
 
-export default function contact() {
+  //handleChange fuction
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  // handleSubmit fuction
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let firstN = document.querySelectorAll(".form-item");
+    firstN.forEach((element) => {
+      element.value = "";
+    });
+    alert("Submition successful!");
+  };
+
   return (
-    <>
-      <h1>contact</h1>
+    <div className="contact">
+      <h1>Contact</h1>
       <form className="form-contact" method="GET" onSubmit={handleSubmit}>
         <label>
           First Name:
-          <input type="text" name="name" value="" onChange={handleChange} />
+          <input
+            className="form-item"
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
         </label>
         <label>
           Last Name:
-          <input type="text" name="name" value="" onChange={handleChange} />
+          <input
+            className="form-item"
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
         </label>
         <label>
           Email:
-          <input type="email" name="email" value="" onChange={handleChange} />
+          <input
+            className="form-item"
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
         </label>
         <label>
           Comment:
-          <textarea name="Comment" value="" onChange={handleChange} />
+          <textarea
+            className="form-item"
+            name="comment"
+            placeholder="Comment"
+            value={formData.comment}
+            onChange={handleChange}
+          />
         </label>
         <button type="submit">Submit</button>
       </form>
-    </>
+    </div>
   );
-}
+};
+export default Contact;
