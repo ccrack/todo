ce73ba2117e4766ca908420849dd11f6e2eefb14
Clebert Civil <clebertcivil01@gmail.com>
2025-06-10 03:58:05 -0400

    create about content on about page

--- src/Pages/about/about.jsx
+++ src/Pages/about/about.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import './about.css'
 
 export default function about() {
   return (
-    <div>about</div>
-  )
+    <div className="about-container">
+      <h1 className="about-title">About</h1>
+      <p className="about-content">
+        Keep things simple with our minimalist to-do list app. Easily create
+        tasks to stay on top of your day. Mark tasks complete with a single tap.
+        Delete what you no longer need. No distractions, just the essentials to
+        stay organized. Perfect for those who value simplicity and focus.
+      </p>
+    </div>
+  );
 }
